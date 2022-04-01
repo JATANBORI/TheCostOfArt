238304f657e60275864b7b02de980d0963c6ad81
John Atanbori <j.atanbori@hull.ac.uk>
2022-04-01 23:38:59 +0100

    initial

---
+++ The Cost of Art/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_Cost_of_Art
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
